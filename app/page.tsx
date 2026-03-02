bde0efc234d0f1270cfa7d909f7c5cc5e4a7ed83
jkarlsss <kolley320@gmail.com>
2026-03-02 16:08:16 +0800

    database setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import { Button } from "../components/ui/button"
+import prisma from "../lib/prisma"
 
-const page = () => {
+const page = async () => {
+
+  const users = await prisma.user.findMany()
+  console.log(users)
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <div className="flex flex-col gap-2">
+        {users.map((user) => (
+          <p key={user.id}>{user.email}</p>
+        ))}
+        </div>
+      
       <Button variant="outline">Click me</Button>
     </div>
   )
