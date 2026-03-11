df9d8089194ad3b8d8ea6aec803590de531337d0
jkarlsss <kolley320@gmail.com>
2026-03-11 09:31:26 +0800

    border-r changed to borde-b

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "./ui/sidebar";
 
 export function AppHeader() {
   return (
-    <header className="flex h-14 shrink-0 items-center gap-2 border-r px-4 bg-background">
+    <header className="flex h-14 shrink-0 items-center gap-2 border-b px-4 bg-background">
       <SidebarTrigger />
     </header>
   )
